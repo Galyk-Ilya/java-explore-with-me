957bc8f0300ff23e4e98d4a58ca96cb37182f737
Илья <galyk.ilya@yandex.ru>
2023-10-08 16:20:47 +0300

    main_svc v1 code style#1

--- ewm-service/src/main/java/ru/practicum/user/dto/UserShortDto.java
+++ ewm-service/src/main/java/ru/practicum/user/dto/UserShortDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
